92cbff05f5b04faec6b7c619a10c135668779ea2
xk44 <38152958+xk44@users.noreply.github.com>
2026-02-17 13:05:07 -0800

    Fix misleading notification labels in space settings

--- commet/lib/ui/pages/settings/categories/space/space_general_settings_page.dart
+++ commet/lib/ui/pages/settings/categories/space/space_general_settings_page.dart
@@ -2,6 +2,7 @@ import 'package:commet/client/client.dart';
 import 'package:commet/client/matrix/matrix_space.dart';
 import 'package:commet/ui/pages/matrix/room_address_settings/matrix_room_address_settings.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
 import '../room/general/room_general_settings_view.dart';
 
@@ -17,6 +18,27 @@ class SpaceGeneralSettingsPage extends StatefulWidget {
 class _SpaceGeneralSettingsPageState extends State<SpaceGeneralSettingsPage> {
   late PushRule pushRule;
 
+  String get labelSpaceSettingsNotifications => Intl.message(
+      "Space notifications",
+      desc: "Label for the notifications section in space settings",
+      name: "labelSpaceSettingsNotifications");
+
+  String get labelSpacePushRuleNotifyAll => Intl.message(
+      "All messages in this space",
+      desc:
+          "Label for the push rule which notifies for all messages in a space",
+      name: "labelSpacePushRuleNotifyAll");
+
+  String get labelSpacePushRuleMentionsAndKeywords => Intl.message(
+      "Mentions & keywords in this space",
+      desc:
+          "Label for the push rule which notifies only for mentions and keywords in a space",
+      name: "labelSpacePushRuleMentionsAndKeywords");
+
+  String get labelSpacePushRuleNone => Intl.message("Mute this space",
+      desc: "Label for muting notifications for a space",
+      name: "labelSpacePushRuleNone");
+
   @override
   void initState() {
     pushRule = widget.space.pushRule;
@@ -30,6 +52,11 @@ class _SpaceGeneralSettingsPageState extends State<SpaceGeneralSettingsPage> {
         RoomGeneralSettingsView(
           pushRule: pushRule,
           onPushRuleChanged: setPushRule,
+          labelRoomSettingsNotificationsOverride: labelSpaceSettingsNotifications,
+          labelPushRuleNotifyAllOverride: labelSpacePushRuleNotifyAll,
+          labelPushRuleMentionsAndKeywordsOverride:
+              labelSpacePushRuleMentionsAndKeywords,
+          labelPushRuleNoneOverride: labelSpacePushRuleNone,
         ),
         const SizedBox(
           height: 10,
